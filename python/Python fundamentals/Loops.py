060393d0d17e5f46dd3b9fa0b5ba3f55060a4edb
jihad090 <shjihad11@gmail.com>
2025-11-28 07:23:20 +0600

    function completed

--- python/Python fundamentals/Loops.py
+++ python/Python fundamentals/Loops.py
@@ -37,3 +37,38 @@ for i in range(10):
 # | else      | executes if loop exits clean |
 
 # -----Advance Looping -----
+# enumerate() => it not only thaverse but also count index (starts from 0)
+for index, fruit in enumerate(fruits):
+  print(index, fruit)
+
+# zip() => Looping over multiple sequesce
+names = ["Jihad", "Fahad", "Amir"]
+scores = [10, 12, 99]
+for name, score in zip(names, scores):
+  print(name, score)
+
+# List comprehension (short powerful way to create list under some rules)
+square = [x*x for x in range(10)]
+print(square)
+
+even = [x for x in range(10) if x%2==0]
+print(even)
+
+# iterating dictionary key
+student = {'name':'Jihad', 'age':'23', 'mobile':'01834373977'}
+student.items() # [('name', 'Jihad'), ('age', '23'), ('mobile', '01834373977')] -> this is a dict_item
+for key,val in student.items():
+  print(key,val)
+
+# looping in reverse
+for i in reversed(range(5)):
+  print(i)
+
+# | Feature              | Use                       |
+# | -------------------- | ------------------------- |
+# | enumerate            | tracking epoch index      |
+# | zip                  | combine features & labels |
+# | list comprehension   | fast preprocessing        |
+# | filtering            | cleaning datasets         |
+# | nested loops         | scanning image pixels     |
+# | dictionary iteration | reading JSON results      |
